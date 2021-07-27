43e190057cf84c6b9389a8343477257537e68053
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2021-07-27 14:58:29 -0300

    Configurado Produto e adicionada anotações

--- src/main/java/br/com/supera/game/store/domain/Product.java
+++ src/main/java/br/com/supera/game/store/domain/Product.java
@@ -1,24 +1,106 @@
 package br.com.supera.game.store.domain;
 
+import java.io.Serializable;
 import java.math.BigDecimal;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
-public class Product {
+public class Product implements Serializable{
 
+	private static final long serialVersionUID = 1L;
+
+	// Variables
 	@Id
-	@GeneratedValue
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	public long id;
-
 	public String name;
-
 	public BigDecimal price;
-
 	public short score;
-
 	public String image;
+	
+	// Constructor of class
+	public Product() {
+		super();
+	}
+
+	// Constructor
+	public Product(long id, String name, BigDecimal price, short score, String image) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.price = price;
+		this.score = score;
+		this.image = image;
+	}
+
+	
+	// Get and Set
+	
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public BigDecimal getPrice() {
+		return price;
+	}
+
+	public void setPrice(BigDecimal price) {
+		this.price = price;
+	}
+
+	public short getScore() {
+		return score;
+	}
+
+	public void setScore(short score) {
+		this.score = score;
+	}
+
+	public String getImage() {
+		return image;
+	}
+
+	public void setImage(String image) {
+		this.image = image;
+	}
+
+	// Hash and equal
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (int) (id ^ (id >>> 32));
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Product other = (Product) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
 
 }
